a2cc5040025f02ba321cdb401242f0b0f9d4dcc3
Elena <e.l.e.n.a_68@mail.ru>
2019-04-21 16:03:37 +0400

    Реализована простая перестановка без ключа

---
+++ WithoutKey/WithoutKey/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WithoutKey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите текст: ");
+            string text = Console.ReadLine();
+            text = text.Replace(" ", "");
+            Console.Write("Введите количество столбцов: ");
+            int columns = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(text);
+            string[] Array = new string[columns];
+            int k = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                Array[k++] += text[i];
+                if (k == columns)
+                {
+                    k = 0;
+                }
+            }
+            string encrypted ="";
+            for(int i = 0; i < columns; i++)
+            {
+
+                encrypted += Array[i];
+                
+            }
+            for (int i = 0; i < encrypted.Length; i++)
+            {
+                Console.Write(Char.ToUpper(encrypted[i]));
+                k++;
+                if (k == 5 && i != encrypted.Length-1)
+                {
+                    Console.Write(" ");
+                    k = 0;
+
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
