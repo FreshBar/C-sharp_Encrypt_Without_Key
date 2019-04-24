2a11718f66998b683f04f4408c1f2c4b507c835f
Elena <e.l.e.n.a_68@mail.ru>
2019-04-24 15:39:08 +0400

    Добавлены проверки на корректность вводимых данных

--- WithoutKey/WithoutKey/Program.cs
+++ WithoutKey/WithoutKey/Program.cs
@@ -10,42 +10,61 @@ namespace WithoutKey
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите текст: ");
-            string text = Console.ReadLine();
-            text = text.Replace(" ", "");
-            Console.Write("Введите количество столбцов: ");
-            int columns = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine(text);
-            string[] Array = new string[columns];
-            int k = 0;
-            for (int i = 0; i < text.Length; i++)
+            string check ="y";
+            do
             {
-                Array[k++] += text[i];
-                if (k == columns)
+                Console.Write("Введите текст: ");
+                string text = Console.ReadLine();
+                text = text.Replace(" ", "");
+                if (text == "")
                 {
-                    k = 0;
+                    Console.WriteLine("Ничего не введено!");
+                    continue;
                 }
-            }
-            string encrypted ="";
-            for(int i = 0; i < columns; i++)
-            {
-
-                encrypted += Array[i];
-                
-            }
-            for (int i = 0; i < encrypted.Length; i++)
-            {
-                Console.Write(Char.ToUpper(encrypted[i]));
-                k++;
-                if (k == 5 && i != encrypted.Length-1)
+                Console.Write("Введите количество столбцов: ");
+                int columns = Convert.ToInt32(Console.ReadLine());
+                if (columns <= 0)
                 {
-                    Console.Write(" ");
-                    k = 0;
-
+                    Console.WriteLine("Неправильно введено количество столбцов!");
+                    continue;
                 }
 
+                //Console.WriteLine(text);
+                string[] Array = new string[columns];
+                int k = 0;
+                for (int i = 0; i < text.Length; i++)
+                {
+                    Array[k++] += text[i];
+                    if (k == columns)
+                    {
+                        k = 0;
+                    }
+                }
+
+                string encrypted = "";
+                for (int i = 0; i < columns; i++)
+                {
+
+                    encrypted += Array[i];
+
+                }
+                k = 0;
+                for (int i = 0; i < encrypted.Length; i++)
+                {
+                    Console.Write(Char.ToUpper(encrypted[i]));
+                    k++;
+                    if (k == 5 && i != encrypted.Length - 1)
+                    {
+                        Console.Write(" ");
+                        k = 0;
+
+                    }
+
+                }
+                Console.WriteLine("\nХотите продолжить? (y/n)");
+                check = Convert.ToString(Console.ReadLine());
             }
-            Console.ReadLine();
+            while (check == "y");
         }
     }
 }
